8dbfc6d4ce11563b853635b4631beb4975bbad60
Lucas Vaz <vazdelimalucas@gmail.com>
2022-05-18 19:19:42 -0300

    Created number.py file

---
+++ numbers.py
@@ -0,0 +1,13 @@
+#Using python as a calculator
+
+'''
+We can use the interpreter to take an input and return an output
+
+Aritmetic Operators:
+    We have a whle bunch of operators at our disposal
+    + Addition
+    - Subtraction
+    * Multiplication
+    / Division
+    // Floor Division
+    
